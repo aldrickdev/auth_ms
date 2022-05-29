4e59b671afa6177a240309a9c28d1a64a1abb92e
aldrickdev <aldrickdev@gmail.com>
2022-05-29 12:23:54 -0400

    Tests for /protected endpoint complete

--- tests/test_auth_ms.py
+++ tests/test_auth_ms.py
@@ -8,10 +8,36 @@ client = TestClient(app)
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
 
 
 def test_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_protected():
+    response = client.get("/protected")
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
+
+    """
+    curl -X 'GET' \
+    'http://localhost:3000/protected' \
+    -H 'accept: application/json' \
+    -H 'Authorization: Bearer johndoe'
+    """
+    response = client.get(
+        "/protected",
+        headers={"accept": "application/json", "Authorization": "Bearer johndoe"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {"token": "johndoe"}
+
+    response = client.get(
+        "/protected",
+        headers={"accept": "application/json", "Authorization": "Bearer bad_token"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {"token": "bad_token"}
