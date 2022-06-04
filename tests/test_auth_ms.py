81d4ee7edb4a0a727997b3f3300c1d45f6cf1804
aldrickdev <aldrickdev@gmail.com>
2022-06-04 11:24:52 -0400

    Updated tests

--- tests/test_auth_ms.py
+++ tests/test_auth_ms.py
@@ -18,6 +18,7 @@ def test_main():
 
 
 def test_protected():
+    endpoint = "/protected"
     response = client.get("/protected")
     assert response.status_code == 401
     assert response.json() == {"detail": "Not authenticated"}
@@ -29,18 +30,27 @@ def test_protected():
     -H 'Authorization: Bearer johndoe'
     """
     response = client.get(
-        "/protected",
+        endpoint,
         headers={"accept": "application/json", "Authorization": "Bearer johndoe"},
     )
     assert response.status_code == 200
     assert response.json() == {"token": "johndoe"}
 
     response = client.get(
-        "/protected",
+        endpoint,
         headers={"accept": "application/json", "Authorization": "Bearer bad_token"},
     )
     assert response.status_code == 200
     assert response.json() == {"token": "bad_token"}
+
+    # Test protected with a good token
+    good_token = get_good_token()
+    response = client.get(
+        endpoint,
+        headers={"accept": "application/json", "Authorization": f"Bearer {good_token}"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {"token": "johndoe"}
 
 
 def test_token():
@@ -77,6 +87,11 @@ def test_token():
     -H 'accept: application/json' \
     -H 'Content-Type: application/x-www-form-urlencoded' \
     -d 'grant_type=&username=invaliduser&password=password&scope=&client_secret='
+    ---
+    http --form :3000/token \
+    accept:application/json
+    Content-Type:application/x-www-form-urlencoded \
+    grant_type= username=invaliduser password=password scope= client_secret=
     """
     response = client.post(
         endpoint,
@@ -100,3 +115,78 @@ def test_token():
     )
     assert response.status_code == 200
     assert response.json() == {"access_token": "johndoe", "token_type": "bearer"}
+
+
+def test_users_me():
+    endpoint = "/users/me"
+
+    """
+    Reference:
+    curl -X 'GET' \
+    'http://localhost:3000/users/me' \
+    ---
+    http :3000/users/me
+    """
+    response = client.get(endpoint)
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
+
+    """
+    curl -X 'GET' \
+    'http://localhost:3000/users/me' \
+    -H 'accept: application/json' \
+    -H 'Authorization: Bearer invalid_token'
+    ---
+    http :3000/users/me \
+    accept:application/json Authorization:"Bearer invalid_token"
+    """
+    response = client.get(
+        endpoint,
+        headers={
+            "accept": "application/json",
+            "Authorization": "Bearer invalid_token",
+        },
+    )
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid Authentication Credentials"}
+
+    """
+    curl -X 'GET' \
+    'http://localhost:3000/users/me' \
+    -H 'accept: application/json' \
+    -H 'Authorization: Bearer johndoe'
+    ---
+    http :3000/users/me \
+    accept:application/json Authorization:"Bearer johndoe"
+    """
+    response = client.get(
+        endpoint,
+        headers={
+            "accept": "application/json",
+            "Authorization": "Bearer johndoe",
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "disabled": False,
+        "email": "johndoe@example.com",
+        "full_name": "John Doe",
+        "hashed_password": "fakehashedsecret",
+        "username": "johndoe",
+    }
+
+
+def get_good_token() -> str:
+    response = client.post(
+        "/token",
+        headers={
+            "accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data="grant_type=&username=johndoe&password=secret&scope=&client_secret=",
+    )
+
+    return response.json().get("access_token")
